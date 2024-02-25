46563894c431f396a2adff5a7e7c1f368e29b5f6
armaan-crypto <armaanahmed08@gmail.com>
2024-02-25 04:57:35 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,23 +1,32 @@
 from flask import Flask, jsonify, request
 import google.generativeai as genai
-
 import yfinance as yf
-
 import numpy as np
 from datetime import datetime, timedelta
+import requests
+
 
 app = Flask(__name__)
 
 
 
 GOOGLE_API_KEY = 'AIzaSyBEH0mo0PAabKy80amr5_nzBf6Gh5VdiAA'
+api_key = "0WCHDS8EX4GHOOC4"
+
+
 
 def describe(stock):
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content("Generate 3 short points describing this company: " + stock)
-    
     return {"description": response.text}
+
+
+
+    percentage_change = (current_price - stock_data['Close'][-1]) 
+    
+    # Check if the stock is increasing
+    return {"displacement" : round(percentage_change,2)}
 
 def riskScore(symbol):
     current_date_time = datetime.now()
@@ -38,36 +47,36 @@ def riskScore(symbol):
     final_based = pow(0.5, range_based_volatility)
 
 
-    #Beta Value 
-    beta_value = yf.Ticker(symbol).info['beta']
+    #Beta Value
+    try:
+        beta_value = yf.Ticker(symbol).info['beta']
+        finalbeta = 0
+        if (beta_value == 1):
+            finalbeta = 0.5 
+        if (beta_value < 0): 
+            finalbeta = pow(0.5, 1+abs(beta_value))
+        else:
+            finalbeta = pow(0.5, beta_value-1)
 
-    finalbeta = 0
-    if (beta_value == 1):
-        finalbeta = 0.5 
-    if (beta_value < 0): 
-        finalbeta = pow(0.5, 1+abs(beta_value))
-    else:
-        finalbeta = pow(0.5, beta_value-1)
+        if (finalbeta > 0.6): 
+            weightage_returns = 0.5
+            weightage_volatility = 0.1
+            weightage_beta = 0.4
+        else: 
+            weightage_beta = .8
+            weightage_volatility = .025
+            weightage_returns = .175
 
-    if (finalbeta > 0.6): 
-        weightage_returns = 0.5
-        weightage_volatility = 0.1
-        weightage_beta = 0.4
-    else: 
-        weightage_beta = .8
-        weightage_volatility = .025
-        weightage_returns = .175
+        overall_risk_factor = round(
+            (weightage_returns * finalstd) +
+            (weightage_volatility * final_based) +
+            (weightage_beta * finalbeta), 2
+        ) * 100
 
-    overall_risk_factor = round(
-        (weightage_returns * finalstd) +
-        (weightage_volatility * final_based) +
-        (weightage_beta * finalbeta), 2
-    ) * 100
+        return {"risk": overall_risk_factor}
 
-    return {"risk": overall_risk_factor}
-
-
-
+    except KeyError:
+        return None
 
 @app.route('/')
 def home():
@@ -78,7 +87,16 @@ def describe_api():
     stock_symbol = request.args.get('symbol')
     description = describe(stock_symbol)
     score = riskScore(stock_symbol)
-    return jsonify(description, score)
+
+    description.update(score)
+
+    return jsonify(description)
+
+@app.route('/api/score', methods=['GET'])
+def score_api():
+    stock_symbol = request.args.get('symbol')
+    description = riskScore(stock_symbol)
+    return jsonify(description)
 
 if __name__ == '__main__':
     app.run(debug=True)
