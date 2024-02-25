c22f79083b43a3d10f3029624abe8e44d7ac7cb3
armaan-crypto <armaanahmed08@gmail.com>
2024-02-25 03:40:31 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from yahoo_fin import stock_info
+import google.generativeai as genai
 
 app = Flask(__name__)
 
+
+
+GOOGLE_API_KEY = 'AIzaSyBEH0mo0PAabKy80amr5_nzBf6Gh5VdiAA'
+
+def describe(stock):
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content("Generate 3 short points describing this company: " + stock)
+    return {"description": response.text}
+
+
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Go to /api/describe'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/api/describe', methods=['GET'])
+def describe_api():
+    stock_symbol = request.args.get('symbol')
+    description = describe(stock_symbol)
+    return jsonify(description)
+
+if __name__ == '__main__':
+    app.run(debug=True)
